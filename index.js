200a05d236399de4e9204e4f44a63b7d5e01e42d
PRABHAKAR SINGH <prabhakarsingh63915@gmail.com>
2025-07-02 13:06:17 +0530

    🔧 Update socket URL and CORS settings for deployment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://dilse-frontend.vercel.app",
     methods: ["GET", "POST"],
   },
 });
